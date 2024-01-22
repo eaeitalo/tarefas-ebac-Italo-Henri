c4f40c0a4d8bbaf874de477ccc89490c8b34141c
unknown <italohenri.dev@gmail.com>
2024-01-22 16:52:34 -0300

    promeiro commit

--- src/br/com/ihenri/PrimeiraClasse.java
+++ src/br/com/ihenri/PrimeiraClasse.java
@@ -3,6 +3,6 @@ package br.com.ihenri;
 public class PrimeiraClasse {
 
     public static void main(String args[]) {
-        System.out.print("Hello World, Italo");
+        System.out.print("Hello Italo");
     }
 }
